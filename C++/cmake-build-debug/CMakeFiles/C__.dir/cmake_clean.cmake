89fc2b99350117ca42b646ed86c152b9d45c4993
shizhenjun <178823461@qq.com>
2017-11-23 17:08:18 +0800

    init
    初建工程

---
+++ C++/cmake-build-debug/CMakeFiles/C__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__.dir/main.cpp.obj"
+  "C__.pdb"
+  "C__.exe"
+  "C__.exe.manifest"
+  "libC__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
